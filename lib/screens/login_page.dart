dee184fb4ff3476a1081070baa457a2c8ded05df
Srj912 <Sreeraj912@yahoo.com>
2022-06-26 19:16:16 +0530

    updates of Invalid Cases

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,6 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:trailapp/our_components.dart';
 import 'package:trailapp/our_screens.dart';
 
+String getLoginMessageFromErrorCode() {
+  var errorCode;
+  switch (errorCode) {
+    case "ERROR_WRONG_PASSWORD":
+    case "wrong-password":
+      return "Wrong email/password combination.";
+    case "ERROR_USER_NOT_FOUND":
+    case "user-not-found":
+      return "No user found with this email.";
+    case "ERROR_USER_DISABLED":
+    case "user-disabled":
+      return "User disabled.";
+    case "ERROR_TOO_MANY_REQUESTS":
+    case "operation-not-allowed":
+      return "Too many requests to log into this account.";
+    case "ERROR_OPERATION_NOT_ALLOWED":
+    case "operation-not-allowed":
+      return "Server error, please try again later.";
+    case "ERROR_INVALID_EMAIL":
+    case "invalid-email":
+      return "Email address is invalid.";
+    default:
+      return "Login failed. Please try again.";
+  }
+}
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -15,66 +41,89 @@ final _auth = FirebaseAuth.instance;
 class _LoginPageState extends State<LoginPage> {
   String? email;
   String? password;
+
+  get errorCode => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Container(
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           color: Colors.redAccent,
           image: DecorationImage(
             colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.1), BlendMode.dstATop),
+                Colors.black.withOpacity(0.1), BlendMode.dstATop),
             image: AssetImage('assets/mountains.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value){
-                email=value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
-              )),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password=value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password'
-              )),
-            SizedBox(
-              height: 24.0,
-            ),
-            MyButton(colour: Colors.orange, title: 'Log In',
-                onPressed: () async{
-              try{
-                final user = await _auth.signInWithEmailAndPassword(email: email!, password: password!);
-                if (user != null){
-                  Navigator.pushNamed(context, 'home');
-                }
-              } catch (e) {
-                print(e);
-              }
-                }),
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your email',
+                  )),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password')),
+              SizedBox(
+                height: 24.0,
+              ),
+              MyButton(
+                  colour: Colors.orange,
+                  title: 'Log In',
+                  onPressed: () async {
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email!, password: password!);
+                      if (user != null) {
+                        Navigator.pushNamed(context, 'home');
+                      }
+                    } catch (error) {
+                      var errorMessage = getLoginMessageFromErrorCode();
+                      print(error);
+                      _showSnackbar(context, errorMessage);
+                    }
+                  }),
+            ],
+          ),
         ),
       ),
-      ),
     );
+  }
+  void _showSnackbar(BuildContext context,String text){
+    final snackBar = SnackBar(
+      behavior: SnackBarBehavior.floating,
+      duration: const Duration(seconds: 2),
+      content:
+      Text(
+        text,
+        style:
+        const TextStyle(
+          fontFamily: 'Titlefont3',
+          color: Colors.white
+        ),
+      ),
+      backgroundColor: Colors.orangeAccent,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -2,19 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:trailapp/our_components.dart';
 
+String getSignupMessageFromErrorCode() {
+  var errorCode;
+  switch (errorCode) {
+    case "firebase_auth/weak-password":
+      return "Password should be at least 6 characters ";
+    case "ERROR_EMAIL_ALREADY_IN_USE":
+    case "account-exists-with-different-credential":
+    case "email-already-in-use":
+      return "Email already used. Go to login page.";
+    case "ERROR_USER_DISABLED":
+    case "user-disabled":
+      return "User disabled.";
+    case "ERROR_TOO_MANY_REQUESTS":
+    case "operation-not-allowed":
+      return "Too many requests to log into this account.";
+    case "ERROR_OPERATION_NOT_ALLOWED":
+    case "operation-not-allowed":
+      return "Server error, please try again later.";
+    case "ERROR_INVALID_EMAIL":
+    case "invalid-email":
+      return "Email address is invalid.";
+    default:
+      return "Signup failed. Please try again.";
+  }
+}
+
 const kTextFieldDecoration = InputDecoration(
   hintText: 'Enter a value',
   hintStyle: TextStyle(color: Colors.blueGrey),
-  contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
   border: OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
   enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.amber,width: 1.0),
+    borderSide: BorderSide(color: Colors.amber, width: 1.0),
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
   focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.amber,width: 2.0),
+    borderSide: BorderSide(color: Colors.amber, width: 2.0),
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
   ),
 );
@@ -30,65 +56,83 @@ class _SignupPageState extends State<SignupPage> {
   final _auth = FirebaseAuth.instance;
   String? email;
   String? password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Container(
+      body: Container(
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           color: Colors.redAccent,
           image: DecorationImage(
             colorFilter: ColorFilter.mode(
-             Colors.black.withOpacity(0.1), BlendMode.dstATop),
+                Colors.black.withOpacity(0.1), BlendMode.dstATop),
             image: AssetImage('assets/mountains.jpg'),
             fit: BoxFit.cover,
-         ),
+          ),
         ),
-       child: Padding(
+        child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value){
-                  email=value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your email'
-                )),
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your email')),
               SizedBox(
                 height: 8.0,
               ),
               TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value){
-                  password=value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your passsword'
-                )),
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your passsword')),
               SizedBox(
                 height: 24.0,
               ),
-              MyButton(colour: Colors.amber, title: 'Sign Up',
+              MyButton(
+                  colour: Colors.amber,
+                  title: 'Sign Up',
                   onPressed: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(email: email!, password: password!);
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, 'home');
-                  }
-                } catch (e){
-                  print(e);
-                }
+                    try {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email!, password: password!);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, 'home');
+                      }
+                    } catch (error) {
+                      var errorMessage = getSignupMessageFromErrorCode();
+                      print(error);
+                      _showSnackbar(context, errorMessage);
+                    }
                   })
             ],
           ),
         ),
-       ),
+      ),
     );
+  }
+
+  void _showSnackbar(BuildContext context, String text) {
+    final snackBar = SnackBar(
+      behavior: SnackBarBehavior.floating,
+      duration: const Duration(seconds: 2),
+      content: Text(
+        text,
+        style: const TextStyle(fontFamily: 'Titlefont3', color: Colors.white),
+      ),
+      backgroundColor: Colors.orangeAccent,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
